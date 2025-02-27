787334e73df925b2c7c8ff179de799cca0d8729f
Jinninni040507 <moni.oliveras05@gmail.com>
2025-02-27 18:48:57 +0100

    enhance: primeras configuraciones de i18

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import englishContent from "./lang/en.json";
+import spanishContent from "./lang/es.json";
 
-createRoot(document.getElementById('root')!).render(
+i18n.use(initReactI18next).init({
+  resources: {
+    en: englishContent,
+    es: spanishContent,
+  },
+
+  fallbackLng: "en",
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
