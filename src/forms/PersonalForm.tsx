719949a5b55ab22e7e299716e5f68c0828c43adb
Jinninni040507 <moni.oliveras05@gmail.com>
2025-03-09 13:23:30 +0100

    #6 fix: birthdateValidation regex implemented & imports fixed

--- src/forms/PersonalForm.tsx
+++ src/forms/PersonalForm.tsx
@@ -1,92 +1,83 @@
-import { useEffect, useState } from "react"
-import Input from "../components/Input"
+import { useEffect, useState } from "react";
+import Input from "../components/Input";
 import { PersonalFormData } from "../interfaces/form.interfaces";
 import { validateLength } from "../utils/validation";
 import Select from "../components/Select";
 import Checkbox from "../components/Checkbox";
 
 function PersonalForm() {
-    const [formData, setFormData] = useState<PersonalFormData>({
-        name: "",
-        email: "",
-        birthDate: "",
-        gender: "",
-        preferences: []
-    });
+  const [formData, setFormData] = useState<PersonalFormData>({
+    name: "",
+    email: "",
+    birthDate: "",
+    gender: "",
+    preferences: [],
+  });
 
-    useEffect(() => {
-        console.log(formData)
-    },[formData.preferences])
+  useEffect(() => {
+    console.log(formData);
+  }, [formData, formData.preferences]);
 
-    const handlePreferencesChanges = (option: 'Leer' | 'Deportes' | 'Viajar' | 'Cine') => {
-        if (!formData.preferences.includes(option))
-            setFormData({
-                ...formData,
-                preferences: [...formData.preferences, option]
-            })
-        else 
-            setFormData({
-                ...formData,
-                preferences: formData.preferences.filter((opt => opt !== option))
-            })
-    }
+  const handlePreferencesChanges = (
+    option: "Leer" | "Deportes" | "Viajar" | "Cine"
+  ) => {
+    if (!formData.preferences.includes(option))
+      setFormData({
+        ...formData,
+        preferences: [...formData.preferences, option],
+      });
+    else
+      setFormData({
+        ...formData,
+        preferences: formData.preferences.filter((opt) => opt !== option),
+      });
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target
-        setFormData({...formData, [name]: [value]})
-        validateLength(value, 3, 50)
-    }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: [value] });
+    validateLength(value, 3, 50);
+  };
 
-    return (
-        <div className="flex flex-col gap-5 w-79 p-6 border rounded-3xl">
-            <Input
-                label="Nombre"
-                value={formData.name}
-                name="name"
-                placeholder="Enter your name"
-                type="text"
-                onChange={handleChange}
-            />
-            <Input
-                label="Email"
-                value={formData.email}
-                name="email"
-                placeholder="Enter your email"
-                type="email"
-                onChange={handleChange}
-            />
-            <Input
-                label="Fecha de nacimiento"
-                value={formData.birthDate}
-                name="birthDate"
-                placeholder="mm/dd/yyyy"
-                type="text"
-                onChange={handleChange}
-            />
-            <Select
-                label="Sexo"
-                value={formData.gender}
-                placeholder="Select a option"
-                options={[
-                    'Femenino',
-                    'Masculino',
-                    'Otro'
-                ]}
-            />
-            <Checkbox<'Leer' | 'Deportes' | 'Viajar' | 'Cine'>
-                selectedOptions={formData.preferences}
-                onSelectOption={handlePreferencesChanges}
-                    options={[
-                    'Leer',
-                    'Deportes',
-                    'Viajar',
-                    'Cine'
-                ]}
-                label="Preferencias"
-            />
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-5 w-79 p-6 border rounded-3xl">
+      <Input
+        label="Nombre"
+        value={formData.name}
+        name="name"
+        placeholder="Enter your name"
+        type="text"
+        onChange={handleChange}
+      />
+      <Input
+        label="Email"
+        value={formData.email}
+        name="email"
+        placeholder="Enter your email"
+        type="email"
+        onChange={handleChange}
+      />
+      <Input
+        label="Fecha de nacimiento"
+        value={formData.birthDate}
+        name="birthDate"
+        placeholder="mm/dd/yyyy"
+        type="text"
+        onChange={handleChange}
+      />
+      <Select
+        label="Sexo"
+        value={formData.gender}
+        placeholder="Select a option"
+        options={["Femenino", "Masculino", "Otro"]}
+      />
+      <Checkbox<"Leer" | "Deportes" | "Viajar" | "Cine">
+        selectedOptions={formData.preferences}
+        onSelectOption={handlePreferencesChanges}
+        options={["Leer", "Deportes", "Viajar", "Cine"]}
+        label="Preferencias"
+      />
+    </div>
+  );
 }
-export default PersonalForm
-
-
+export default PersonalForm;
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,27 +1,30 @@
-export function validateLength(value: string, min: number, max?: number): boolean {
-    if (max === undefined)
-        return value.length >= min
+export function validateLength(
+  value: string,
+  min: number,
+  max?: number
+): boolean {
+  if (max === undefined) return value.length >= min;
 
-    return value.length >= min && value.length <= max
+  return value.length >= min && value.length <= max;
 }
 
 export function validateAge(yearOfBirth: number) {
-    const date = new Date;
-    const year = date.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
 
-    return year - yearOfBirth < 17 
+  return year - yearOfBirth < 17;
 }
 
 export function validateBirthDateFormat(birthDate: string) {
-    const birthDateRegex = //;
-    return birthDateRegex.test(birthDate);
+  const birthDateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+  return birthDateRegex.test(birthDate);
 }
 
 export function validateEmailFormat(email: string) {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.stucom\.com$/;
-    return emailRegex.test(email)
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.stucom\.com$/;
+  return emailRegex.test(email);
 }
 
 export function validateMaxSelected(selected: string[]) {
-    return selected.length > 2
+  return selected.length > 2;
 }
